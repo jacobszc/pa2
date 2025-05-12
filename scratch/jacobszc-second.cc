4ebf4cf71efc5a918e458a86e761c42ae337fe57
Jacob <jacobszc@buffalo.edu>
2025-05-11 21:25:51 -0400

    more scratch

---
+++ scratch/jacobszc-second.cc
@@ -0,0 +1,129 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "ns3/applications-module.h"
+#include "ns3/core-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/network-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/trace-helper.h"
+
+// Default Network Topology
+//
+//       10.1.1.0
+// n0 -------------- n1   n2   n3   n4
+//    point-to-point  |    |    |    |
+//                    ================
+//                      LAN 10.1.2.0
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE("SecondScriptExample");
+
+int
+main(int argc, char* argv[])
+{
+    bool verbose = true;
+    uint32_t nCsma = 3;
+
+    CommandLine cmd(__FILE__);
+    cmd.AddValue("nCsma", "Number of \"extra\" CSMA nodes/devices", nCsma);
+    cmd.AddValue("verbose", "Tell echo applications to log if true", verbose);
+/// added stuff below ///
+    uint32_t packetSize = 1024;
+    std::string delay = "2ms";
+    uint32_t nPackets = 1;
+    std::string traceFileName = "jacobszc-CSMAdefault";
+ cmd.AddValue("nPackets", "Number of packets", nPackets);
+ cmd.AddValue("PacketSize", "Size of packet", packetSize);
+ cmd.AddValue("Delay", "Delay on the p-p link", delay);
+ cmd.AddValue("traceFileName", "name of .tr to gen", traceFileName); // added this to make generating .tr files faster
+   // added above ///
+   
+    cmd.Parse(argc, argv);
+
+    if (verbose)
+    {
+        LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
+        LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+    }
+
+    nCsma = nCsma == 0 ? 1 : nCsma;
+
+    NodeContainer p2pNodes;
+    p2pNodes.Create(2);
+
+    NodeContainer csmaNodes;
+    csmaNodes.Add(p2pNodes.Get(1));
+    csmaNodes.Create(nCsma);
+
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+    pointToPoint.SetChannelAttribute("Delay", StringValue(delay)); // added for dynamic delay
+
+    NetDeviceContainer p2pDevices;
+    p2pDevices = pointToPoint.Install(p2pNodes);
+
+    CsmaHelper csma;
+    csma.SetChannelAttribute("DataRate", StringValue("100Mbps"));
+    csma.SetChannelAttribute("Delay", TimeValue(NanoSeconds(6560)));
+
+    NetDeviceContainer csmaDevices;
+    csmaDevices = csma.Install(csmaNodes);
+
+    InternetStackHelper stack;
+    stack.Install(p2pNodes.Get(0));
+    stack.Install(csmaNodes);
+
+    Ipv4AddressHelper address;
+    address.SetBase("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer p2pInterfaces;
+    p2pInterfaces = address.Assign(p2pDevices);
+
+    address.SetBase("10.1.2.0", "255.255.255.0");
+    Ipv4InterfaceContainer csmaInterfaces;
+    csmaInterfaces = address.Assign(csmaDevices);
+
+    UdpEchoServerHelper echoServer(9);
+
+    ApplicationContainer serverApps = echoServer.Install(csmaNodes.Get(nCsma));
+    serverApps.Start(Seconds(1.0));
+    serverApps.Stop(Seconds(10.0));
+
+    UdpEchoClientHelper echoClient(csmaInterfaces.GetAddress(nCsma), 9);
+    echoClient.SetAttribute("MaxPackets", UintegerValue(nPackets)); ///<---////changed this to allow npackets rather than constant 1
+    echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+    echoClient.SetAttribute("PacketSize", UintegerValue(packetSize)); // added for dynamic packet size echo
+
+    ApplicationContainer clientApps = echoClient.Install(p2pNodes.Get(0));
+    clientApps.Start(Seconds(2.0));
+    clientApps.Stop(Seconds(10.0));
+
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+
+    pointToPoint.EnablePcapAll("second");
+    csma.EnablePcap("second", csmaDevices.Get(1), true);
+    /// added below /////
+    AsciiTraceHelper ascii;
+
+    pointToPoint.EnableAsciiAll(ascii.CreateFileStream("jacobszc-P2P.tr"));
+    csma.EnableAsciiAll(ascii.CreateFileStream(traceFileName));
+    // added above // 
+    Simulator::Run();
+    Simulator::Destroy();
+    return 0;
+}
---
+++ scratch/jacobszc-third.cc
@@ -0,0 +1,232 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "ns3/applications-module.h"
+#include "ns3/core-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/network-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/ssid.h"
+#include "ns3/yans-wifi-helper.h"
+#include "ns3/netanim-module.h"
+
+// Default Network Topology
+//
+//   Wifi 10.1.3.0
+//                 AP
+//  *    *    *    *
+//  |    |    |    |    10.1.1.0
+// n5   n6   n7   n0 -------------- n1   n2   n3   n4
+//                   point-to-point  |    |    |    |
+//                                   ================
+//                                     LAN 10.1.2.0
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE("ThirdScriptExample");
+/// added func from tutorial file below
+void
+CourseChange(std::string context, Ptr<const MobilityModel> model)
+{
+Vector position = model->GetPosition();
+NS_LOG_UNCOND(context <<
+" x = " << position.x << ", y = " << position.y);
+}
+/// added func from tutorial file above ///////
+
+int
+main(int argc, char* argv[])
+{
+    bool verbose = true;
+    uint32_t nCsma = 3;
+    uint32_t nWifi = 3;
+    uint32_t nPackets = 1;
+    bool tracing = false;
+    std::string xmlFileName = "jacobszc-third-defualt.xml";
+    std::string trFileName = "jacobszc-third-csma-defualt.tr";
+
+    CommandLine cmd(__FILE__);
+    cmd.AddValue("nCsma", "Number of \"extra\" CSMA nodes/devices", nCsma);
+    cmd.AddValue("nWifi", "Number of wifi STA devices", nWifi);
+    cmd.AddValue("verbose", "Tell echo applications to log if true", verbose);
+    cmd.AddValue("tracing", "Enable pcap tracing", tracing);
+    cmd.AddValue("nPackets", "Number of packets to send", nPackets); //added this for nPackets
+    cmd.AddValue("xmlFileName", "name of gen xml", xmlFileName);
+    cmd.AddValue("trFileName", "name of gen tr", trFileName);
+
+    cmd.Parse(argc, argv);
+
+    // The underlying restriction of 18 is due to the grid position
+    // allocator's configuration; the grid layout will exceed the
+    // bounding box if more than 18 nodes are provided.
+    if (nWifi > 18)
+    {
+        std::cout << "nWifi should be 18 or less; otherwise grid layout exceeds the bounding box"
+                  << std::endl;
+        return 1;
+    }
+
+    if (verbose)
+    {
+        LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
+        LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+    }
+
+    NodeContainer p2pNodes;
+    p2pNodes.Create(2);
+
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+
+    NetDeviceContainer p2pDevices;
+    p2pDevices = pointToPoint.Install(p2pNodes);
+
+    NodeContainer csmaNodes;
+    csmaNodes.Add(p2pNodes.Get(1));
+    csmaNodes.Create(nCsma); // changed to crate 5 wifi clients
+
+    CsmaHelper csma;
+    csma.SetChannelAttribute("DataRate", StringValue("100Mbps"));
+    csma.SetChannelAttribute("Delay", TimeValue(NanoSeconds(6560)));
+
+    NetDeviceContainer csmaDevices;
+    csmaDevices = csma.Install(csmaNodes);
+
+    NodeContainer wifiStaNodes;
+    wifiStaNodes.Create(nWifi); /// changed to only create 5
+    NodeContainer wifiApNode = p2pNodes.Get(0);
+
+    YansWifiChannelHelper channel = YansWifiChannelHelper::Default();
+    YansWifiPhyHelper phy;
+    phy.SetChannel(channel.Create());
+
+    WifiMacHelper mac;
+    Ssid ssid = Ssid("ns-3-ssid");
+
+    WifiHelper wifi;
+
+    NetDeviceContainer staDevices;
+    mac.SetType("ns3::StaWifiMac", "Ssid", SsidValue(ssid), "ActiveProbing", BooleanValue(false));
+    staDevices = wifi.Install(phy, mac, wifiStaNodes);
+
+    NetDeviceContainer apDevices;
+    mac.SetType("ns3::ApWifiMac", "Ssid", SsidValue(ssid));
+    apDevices = wifi.Install(phy, mac, wifiApNode);
+
+    MobilityHelper mobility;
+
+    mobility.SetPositionAllocator("ns3::GridPositionAllocator",
+                                  "MinX",
+                                  DoubleValue(0.0),
+                                  "MinY",
+                                  DoubleValue(0.0),
+                                  "DeltaX",
+                                  DoubleValue(5.0),
+                                  "DeltaY",
+                                  DoubleValue(10.0),
+                                  "GridWidth",
+                                  UintegerValue(3),
+                                  "LayoutType",
+                                  StringValue("RowFirst"));
+
+    mobility.SetMobilityModel("ns3::RandomWalk2dMobilityModel",
+                              "Bounds",
+                              RectangleValue(Rectangle(-50, 50, -50, 50)));
+    mobility.Install(wifiStaNodes);
+
+    mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    mobility.Install(wifiApNode);
+
+    InternetStackHelper stack;
+    stack.Install(csmaNodes);
+    stack.Install(wifiApNode);
+    stack.Install(wifiStaNodes);
+
+    Ipv4AddressHelper address;
+
+    address.SetBase("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer p2pInterfaces;
+    p2pInterfaces = address.Assign(p2pDevices);
+
+    address.SetBase("10.1.2.0", "255.255.255.0");
+    Ipv4InterfaceContainer csmaInterfaces;
+    csmaInterfaces = address.Assign(csmaDevices);
+
+    address.SetBase("10.1.3.0", "255.255.255.0");
+    address.Assign(staDevices);
+    address.Assign(apDevices);
+
+    UdpEchoServerHelper echoServer(9);
+
+    ApplicationContainer serverApps = echoServer.Install(csmaNodes.Get(nCsma));
+    serverApps.Start(Seconds(1.0));
+    serverApps.Stop(Seconds(10.0));
+
+    UdpEchoClientHelper echoClient(csmaInterfaces.GetAddress(nCsma), 9);
+    echoClient.SetAttribute("MaxPackets", UintegerValue(nPackets)); // replaced with dynamic packet size from cmd line
+    echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+    echoClient.SetAttribute("PacketSize", UintegerValue(1024));
+
+    ApplicationContainer clientApps = echoClient.Install(wifiStaNodes.Get(nWifi - 1));
+    clientApps.Start(Seconds(2.0));
+    clientApps.Stop(Seconds(10.0));
+
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+
+    Simulator::Stop(Seconds(10.0));
+    //// added below ////
+    
+    mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    mobility.Install(csmaNodes);
+    AnimationInterface anim(xmlFileName); // add new param for step 2 to make this easier
+    anim.SetConstantPosition(p2pNodes.Get(0), 10.0, 20.0); // 
+    anim.SetConstantPosition(p2pNodes.Get(1), 20.0, 20.0);
+    /// replaced static delcartions with loop below so i dont have to keep adding more every time i try to run it with different numbers of paramter cuz thats super annoying.
+
+// CSMA nodes
+   for (uint32_t i = 0; i < csmaNodes.GetN(); i++) {
+    anim.SetConstantPosition(csmaNodes.Get(i), 30.0 + 5 * i, 30.0);
+}
+
+// WiFi STA nodes
+   for (uint32_t i = 0; i < wifiStaNodes.GetN(); i++) {
+    anim.SetConstantPosition(wifiStaNodes.Get(i), 5.0 + 5 * i, 10.0);
+}
+
+
+    AsciiTraceHelper ascii;
+    csma.EnableAsciiAll(ascii.CreateFileStream(trFileName));
+
+/// added above
+    if (tracing)
+    {
+        phy.SetPcapDataLinkType(WifiPhyHelper::DLT_IEEE802_11_RADIO);
+        pointToPoint.EnablePcapAll("third");
+        phy.EnablePcap("third", apDevices.Get(0));
+        csma.EnablePcap("third", csmaDevices.Get(0), true);
+    }
+    /// added below code from tutorial file //////////
+    std::ostringstream oss;
+    oss << "/NodeList/" << wifiStaNodes.Get(nWifi - 3)->GetId()
+        << "/$ns3::MobilityModel/CourseChange";
+    Config::Connect(oss.str(), MakeCallback(&CourseChange));
+/// added above code from tutorial file ///////////
+    Simulator::Run();
+    Simulator::Destroy();
+    return 0;
+}
